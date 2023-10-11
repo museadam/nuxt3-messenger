74c913ddb5fd9813fd4387ac37b7a049f790a3f8
adam270 <musedadam95@gmail.com>
2023-10-10 21:29:54 -0600

    fixed auth error

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const user = useState("currentUser");
-  if (!user.value.id) {
+  if (!user.value?.id) {
     return navigateTo("/login");
   }
 });
